6c58e2c3a51dffa98b01c26885c47c6f05c2dedc
RuinedMango <00rudystx@gmail.com>
2024-03-11 23:59:20 -0800

    advav

--- Assets/Scripts/AI/MediumAI.cs
+++ Assets/Scripts/AI/MediumAI.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MediumAI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public NavMeshAgent agent;
+    public float health;
+    public float dodgeSpeed;
+    private Transform bullet;
+    private Collider2D bulletColliderDetector;
+    // Start is called before the first frame update
     void Start()
     {
-        
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(GameObject.Find("Player").transform.position);
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        gameObject.transform.LookAt(GameObject.Find("Player").transform);
+    }
+    public void Damage(float damage)
+    {
+        health -= damage;
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        bullet = collider.gameObject.transform;
+        agent.gameObject.GetComponent<Rigidbody2D>().AddRelativeForceX(dodgeSpeed, ForceMode2D.Impulse);
+        agent.gameObject.GetComponent<Rigidbody2D>().angularVelocity = Mathf.Clamp(gameObject.GetComponent<Rigidbody2D>().velocity.x, 0f, 2f);
     }
 }
